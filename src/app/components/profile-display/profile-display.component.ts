109738c9bd37b795ae833ecb3f69b534da9cab25
Marian Francescu <marian.francescu98@gmail.com>
2021-04-12 16:10:18 +0300

    Styled the profile component

--- src/app/components/profile-display/profile-display.component.ts
+++ src/app/components/profile-display/profile-display.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import { NgbAlert } from '@ng-bootstrap/ng-bootstrap';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 import { User } from 'src/app/data-models/user';
 import { ApiService } from 'src/app/services/api.service';
 
@@ -10,11 +13,18 @@ import { ApiService } from 'src/app/services/api.service';
 export class ProfileDisplayComponent implements OnInit, OnChanges {
   @Input('user') user: User;
 
+  @ViewChild('successAlert', { static: false }) successAlert: NgbAlert;
+  successMessage = '';
+  private _success = new Subject<string>();
+
+  alertTimeout = 3000;
   username: string;
 
   constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
+    this._success.subscribe(message => this.successMessage = message);
+    this._success.pipe(debounceTime(this.alertTimeout)).subscribe(() => this.successAlert?.close());
   }
 
   ngOnChanges(changes: SimpleChanges) {
@@ -24,7 +34,12 @@ export class ProfileDisplayComponent implements OnInit, OnChanges {
   }
 
   saveProfile(username: string){
-    this.apiService.updateUser(this.user.id, username).subscribe();
-
+    this.apiService.updateUser(this.user.id, username).subscribe(
+      {
+        next: user => {
+          this._success.next(`Username for <b>${user.email}</b> was updated succesfully to: <b>${username}</b>.`)
+        }
+      }
+    );
   }
 }
