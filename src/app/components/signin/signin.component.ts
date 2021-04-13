84c063182dce16c61209bde83a9998d3b9d9978c
Marian Francescu <marian.francescu98@gmail.com>
2021-04-13 09:49:11 +0300

    Restyled some components

--- src/app/components/signin/signin.component.ts
+++ src/app/components/signin/signin.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { NgbAlert } from '@ng-bootstrap/ng-bootstrap';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 import { User } from 'src/app/data-models/user';
 import { ApiService } from 'src/app/services/api.service';
 
@@ -10,6 +13,13 @@ import { ApiService } from 'src/app/services/api.service';
   styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent implements OnInit {
+
+  @ViewChild('dangerAlert', { static: false }) dangerAlert: NgbAlert;
+  dangerMessage = '';
+  private _danger = new Subject<string>();
+
+  alertTimeout = 3000;
+
   signinForm = new FormGroup({
     email: new FormControl('', Validators.required),
     password: new FormControl('', Validators.required),
@@ -24,6 +34,8 @@ export class SigninComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this._danger.subscribe(message => this.dangerMessage = message);
+    this._danger.pipe(debounceTime(this.alertTimeout)).subscribe(() => this.dangerAlert?.close());
   }
 
   validateForm() {
@@ -31,8 +43,13 @@ export class SigninComponent implements OnInit {
       this.signinForm.get('email').value,
       this.signinForm.get('password').value)
       .subscribe({
-        next: () => this.router.navigate(['/messenger']),
-        error: () => this.flag = false  
+        next: () => {
+          this.router.navigate(['/messenger']);
+        },
+        error: () => {
+          this.flag = false;
+          this._danger.next(`Your username or password is invalid!`) 
+        }
       });
      
     }
